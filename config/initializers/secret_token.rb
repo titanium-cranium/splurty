6d1b30144b4f46b0a1401a009c7e98beba34a0ae
titanium-cranium <titaniumcranium@icloud.com>
2015-10-10 12:26:26 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Splurty::Application.config.secret_key_base = '4fdf4032ea45af44224b11d9b518041e2feba8b4a2e7c83684b476ea2b938b88a544349bec3116047d5ac46da167fbe541fe1078078cee0a79d3a0b4999c52d8'
