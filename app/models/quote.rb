49ccf7789a670489bd8fa84981dc13767068d32a
titanium-cranium <titaniumcranium@icloud.com>
2015-12-10 04:27:43 +0000

    TDD for unique_tag complete and built

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,6 +1,18 @@
 class Quote < ActiveRecord::Base
 	validates :author, :presence => true, :length => { in: 3..140 } #remember I had to swap author & saying fields
 	validates :saying, :presence => true, :length => { in: 3..50 }
+
+
+	def unique_tag
+		abbr = []
+		abbr = self.saying.split(" ").collect do |sub_string|
+			sub_string[0]
+		end
+		
+		return abbr.join + '#' + self.id.to_s
+	end
+
+
 end
 
 
--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
-  end
+	test "unique_tag" do
+		quote = Quote.create(:saying => "Dorkus Maximus", :author => "I came, I saw, I dorked")
+		expected = 'DM#' +quote.id.to_s	
+		actual = quote.unique_tag
+		assert_equal expected, actual
+	end
+
 end
